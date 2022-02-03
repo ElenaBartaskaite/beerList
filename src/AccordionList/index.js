c050dbec555aeed417a2253e6fa8c8be66a452d7
Elena Bartaskaite <elena.bartaskaite@gmail.com>
2022-02-03 14:42:18 +0200

    open/close accordion

--- src/AccordionList/index.js
+++ src/AccordionList/index.js
@@ -1,9 +1,33 @@
 import style from './style.module.scss';
+import React, { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 
 function AccordionList(props) {
     return (
         <div className={style.container}>
-            {props.beers? props.beers.map(beer=> <div className={style.listItem}>{beer.name}</div>):null}
+            {props.beers? props.beers.map(beer=> 
+                <ListItem beer = {beer}/>
+            ):null}
+        </div>
+    );
+}
+
+function ListItem(props) {
+    const [open, toggleOpen] = useState(false);
+    return (
+        <div className={style.listItem}>
+            <div className={style.name} onClick={()=>toggleOpen(!open)}>{props.beer.name}{open?<FontAwesomeIcon icon={faMinusCircle} />:<FontAwesomeIcon icon={faPlusCircle} />}</div>
+            {open?
+                <div>
+                    <p>"{props.beer.tagline}"</p>
+                    <p>{props.beer.description}</p>
+                    <p>First brewed: {props.beer.first_brewed}</p>
+                    <p>{props.beer.abv}%</p>
+                </div>
+            :null}
+            
         </div>
     );
 }
